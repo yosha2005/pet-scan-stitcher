c32ab03be1e31649cfe853e88e92d26f40710dda
Johannes Schaller <johannes.schaller@dkfz.de>
2023-04-25 15:05:25 +0200

    Save stitched scan in appropriate location

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ for patient_name in config.PATIENT_NAMES:
     # initialize scans
 
     # get segment information (scan file location and header file location)
-    segment_information, save_path, scan_name = scan_loader.get_segment_information(patient_name)
+    segment_information, path_to_reconstructions, scan_name = scan_loader.get_segment_information(patient_name)
 
     if len(segment_information) == 2:
         upper_segment = scan_loader.Segment(segment_information[0])
@@ -21,7 +21,7 @@ for patient_name in config.PATIENT_NAMES:
             stitched_scan = scan_stitcher.stitch_scans_together(upper_segment, lower_segment, linear_scaling)
 
             # save stitched PET scan
-            scan_saver.save_PET_scan(stitched_scan, config.PATH_TO_DESKTOP)
+            scan_saver.save_PET_scan(stitched_scan, path_to_reconstructions, scan_name)
         
     else:
         print('ERROR: Wrong number of segments was supplied for patient {}.'.format(patient_name))
--- scan_saver.py
+++ scan_saver.py
@@ -1,8 +1,13 @@
 import numpy as np
+import os
 
-def save_PET_scan(scan_array: np.ndarray, save_path):
+def save_PET_scan(scan_array: np.ndarray, path_to_reconstructions, scan_name):
     scan_array_shape = scan_array.shape
     save_array_length = scan_array_shape[0] * scan_array_shape[1] * scan_array_shape[2]
 
+    save_path = os.path.join(path_to_reconstructions, scan_name)
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
+
     scan_array = scan_array.reshape(save_array_length).astype(np.float32)
-    scan_array.tofile(save_path + '\\test.v')
+    scan_array.tofile(save_path + '\\{}.v'.format(scan_name))
